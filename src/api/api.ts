bb9229d248336eeb551ec94ebca48f7d62575a48
Alexander-Verbin <sash.verbin@yandex.ru>
2023-02-21 21:09:40 +0300

    fields for auth

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,5 @@
 import axios from "axios";
+
+const baseUrl = axios.create({
+	baseURL: "http://engine.hotellook.com/api/v2/lookup.json"
+})
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Login.module.scss"
 import {Back} from "../../common/Back/Back";
+import {Form,Input,Button} from "antd";
 
 export const Login:React.FC = () => {
 	return(
@@ -10,6 +11,37 @@ export const Login:React.FC = () => {
 			<div className={style.content__blur}></div>
 			<div className={style.content__form}>
 				<p className={style.content__title}>Simple Hotel Check</p>
+				<Form
+				name={'auth'}
+				autoComplete="off"
+				layout={'vertical'}>
+					<Form.Item
+					label={'Логин'}
+					name={'login'}
+					colon={false}
+					rules={[
+						{
+							type: 'email',
+							message: 'Не правильный формат',
+						},
+					]}>
+						<Input/>
+					</Form.Item>
+					<Form.Item
+						label={'Пароль'}
+						name={'password'}
+						colon={false}
+						rules={[
+							{
+								message: 'Не верный пароль',
+							},
+						]}>
+						<Input.Password/>
+					</Form.Item>
+					<Form.Item>
+						<Button>Войти</Button>
+					</Form.Item>
+				</Form>
 			</div>
 		</div>
 		</>
